ae938e6741fe48006d083a46e93fc87654c4f8a4
Allan Nunes <112179619+DevNunesFerreira@users.noreply.github.com>
2022-12-15 16:49:24 -0300

    Add files via upload

--- listaDeExerciciosManzano/AreaResidencia.java
+++ listaDeExerciciosManzano/AreaResidencia.java
@@ -1,31 +1,35 @@
-package manzanoEnquanto;
+package manzanoRepita;
 
 import java.util.Scanner;
 
-public class E09_AreaResidencia {
+public class E08_AreaResidencia {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		char resposta = 'S';
-		int area;
-		int areaTotal = 0;
-		while (resposta=='S') {
-			System.out.println("Informe o nome do cômodo: ");
+		char resposta = 's';
+		byte altura;
+		byte comprimento;
+		int areaResidencia;
+		int acumuladora = 0;
+		do {
+			
+			System.out.println("Informe o nome do comodo: ");
 			String comodo = sc.next();
-			System.out.println("Informe a base do"+comodo+" : ");
-			byte base = sc.nextByte();
-			System.out.println("Informe a altura do"+comodo+" : ");
-			byte altura = sc.nextByte();
-			area = base*altura;
-			areaTotal += area;
-			System.out.println("Possui mais algum cômodo?");
+			System.out.println("Informe a altura em metros do "+comodo);
+			altura = sc.nextByte();
+			System.out.println("Informe o comprimento em metros do "+comodo);
+			comprimento = sc.nextByte();
+			areaResidencia = comprimento*altura;
+			acumuladora += areaResidencia;
+			
+			System.out.println("Possui mais algum comodo?");
 			resposta = sc.next().charAt(0);
-		}
-		System.out.println("A área total de sua residência é: "+areaTotal+"m²");
+			
+		} while (resposta == 's');
+		System.out.println("A área total de sua residência é: "+acumuladora+"m²");
 		sc.close();
-
 	}
 
 }
